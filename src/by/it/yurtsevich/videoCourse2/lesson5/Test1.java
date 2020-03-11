be4fb70172c7bc3245dccc15954a45d0199817cb
yurtsevichK <nicecurls@icloud.com>
2020-03-11 14:55:21 +0300

    lesson 5

--- src/by/it/yurtsevich/videoCourse2/lesson5/Test1.java
+++ src/by/it/yurtsevich/videoCourse2/lesson5/Test1.java
@@ -1,5 +1,30 @@
 package by.it.yurtsevich.videoCourse2.lesson5;
 
-public class Test1 {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Test1 {
+    public static void main(String[] args) {
+        // Создаем хешмеп
+        Map<Integer, String> map = new HashMap<>();
+        // заполняем меп (ключ = значение)
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        System.out.println(map);
+
+        // Ключ три принимает новое значение(перезаписывается) и выводит его !ДУБЛИКАТОВ БЫТЬ НЕ МОЖЕТ
+        map.put(3,"Другое значение");
+        System.out.println(map);
+
+        // Выводит желаемое значение метод гет указав его ключ
+        System.out.println(map.get(1));
+
+        // Прохождение по мепу с помощью цикла форич
+        for (Map.Entry<Integer,String> entry: map.entrySet()){
+            // Выводим на экран ключ + значение
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+    }
 }
