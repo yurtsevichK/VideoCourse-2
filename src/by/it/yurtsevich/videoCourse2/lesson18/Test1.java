6a4c5d7fa7f459db030fa3bb535eab11b64bc139
yurtsevichK <nicecurls@icloud.com>
2020-03-18 22:19:58 +0300

    lesson 18

---
+++ src/by/it/yurtsevich/videoCourse2/lesson18/Test1.java
@@ -0,0 +1,41 @@
+package by.it.yurtsevich.videoCourse2.lesson18;
+
+public class Test1 {
+    private int counter;
+
+    public static void main(String[] args) throws InterruptedException {
+        Test1 test = new Test1();
+        test.doWork();
+    }
+
+    public synchronized void increment() {
+        counter++;
+    }
+
+    public void doWork() throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    increment();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    increment();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(counter);
+    }
+}
