8f73d448c846b161d1dbe9373d81c95d19545e2c
yurtsevichK <nicecurls@icloud.com>
2020-03-19 18:26:41 +0300

    lesson 19

---
+++ src/by/it/yurtsevich/videoCourse2/lesson19/Test1.java
@@ -0,0 +1,87 @@
+package by.it.yurtsevich.videoCourse2.lesson19;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Test1 {
+    private int counter;
+
+    public static void main(String[] args) throws InterruptedException {
+        Worker worker = new Worker();
+        worker.main();
+
+    }
+}
+
+class Worker {
+    Random random = new Random();
+
+    Object lock1 = new Object();
+    Object lock2 = new Object();
+
+    private List<Integer> list = new ArrayList<>();
+    private List<Integer> list2 = new ArrayList<>();
+
+    public void addToList1() {
+        synchronized (lock1) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list.add(random.nextInt(100));
+        }
+    }
+
+    public void addtoList2() {
+        synchronized (lock2) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list2.add(random.nextInt(100));
+        }
+    }
+
+    public void work() throws InterruptedException {
+        addToList1();
+        addtoList2();
+    }
+
+    public void main() throws InterruptedException {
+        long before = System.currentTimeMillis();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    work();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    work();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+        long after = System.currentTimeMillis();
+
+        System.out.println(after - before);
+        System.out.println("List1:" + list.size());
+        System.out.println("List2:" + list2.size());
+    }
+}
+
